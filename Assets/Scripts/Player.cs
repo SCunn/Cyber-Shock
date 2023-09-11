6bddd37705e047b8b191d124f1142d21b356f46e
SCunn <43576829+SCunn@users.noreply.github.com>
2023-09-12 00:11:18 +0100

    adding particle effects to gun fire

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,7 +15,10 @@ public class Player : MonoBehaviour
 
     public GameObject bullet;
     public Transform firePosition;
+
+    public GameObject muzzleFlash, bulletHole;
     
+
     // Start is called before the first frame update
     void Start()
     {
@@ -51,6 +54,7 @@ public class Player : MonoBehaviour
                 firePosition.LookAt(myCameraHead.position + (myCameraHead.forward * 50f));
             }
 
+            Instantiate(muzzleFlash, firePosition.position, firePosition.rotation, firePosition);
             Instantiate(bullet, firePosition.position, firePosition.rotation);
         }
     }
